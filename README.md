1569aa26643d4d29b3efe8234b5b101fecf79790
Sérgio Freitas <sjcsgfreitas@hotmail.com>
2021-10-07 00:03:24 +0100

    Minor scope changes for more examples.

--- README.md
+++ README.md
@@ -14,4 +14,11 @@ Do the following curls:
 - Increment counter: `curl -I --request POST --url 'http://localhost:3000/counter'`
 - Check value of counter: `curl --request GET --url 'http://localhost:3000/counter'`
 
+## Behaviors
+
+If you add query parameters to the request, the request will respond with a 400 and it will log an error.
+All requests are logged with a unique-id per request. If the same request has more than one log, the id will be the same.
+The ID is generated on demand, so any service that depends on RANDOM_UUID will always have a different one per instance.
+The counter is configured as a singleton, as long as the app is alive it will keep its state globally.
+
 Enjoy!
--- src/wire/add-services.ts
+++ src/wire/add-services.ts
@@ -4,15 +4,15 @@ import { v4 as uuidv4 } from 'uuid';
 import { ConsoleLogger } from '../services/console-logger';
 import { Counter } from '../services/counter';
 
-export const REQUEST_ID = 'RequestId';
+export const RANDOM_UUID = 'RequestId';
 export const CONSOLE = 'Console';
 export const LOGGER = 'Logger';
 export const COUNTER_SERVICE = 'Counter';
 
 export function registerServices(builder: Builder): Builder {
   return builder
-    .add(REQUEST_ID, () => uuidv4(), ScopeType.Transient)
+    .add(RANDOM_UUID, () => uuidv4(), ScopeType.OnDemand)
     .add(CONSOLE, () => console, ScopeType.SingleInstance)
-    .addType(LOGGER, ConsoleLogger, [REQUEST_ID, CONSOLE], ScopeType.Transient)
+    .addType(LOGGER, ConsoleLogger, [RANDOM_UUID, CONSOLE], ScopeType.Transient)
     .addType(COUNTER_SERVICE, Counter, [], ScopeType.SingleInstance);
 }
